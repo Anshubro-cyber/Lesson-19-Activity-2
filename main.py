3030f2cf18126d53f5823272d40cbd588284a403
Anshubro-cyber <akshatagrawal0831@gmail.com>
2024-10-17 19:16:33 +0530

    Lssn-19 Act-2

---
+++ main.py
@@ -0,0 +1,11 @@
+#For function1, the Alogorithm goes like this:
+#(4*5)/2
+#So,the number of iterations will be 1.
+
+#For function2 the Alogorithm goes like:
+#1+2+3+4
+#So,the number of iterations will be 1+1+1+1=4
+
+#For function3 the Alogorithm goes like:
+#1+(1+1)+(1+1+1)+(1+1+1+1)
+#So,the number of iterations will be 1+2+3+4=10
